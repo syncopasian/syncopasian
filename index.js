8dc1c2121d33798245cbd983c72e5b12362527bf
Syncopasian <syncopasian@mit.edu>
2015-10-18 23:27:57 -0400

    https

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var public_dir = __dirname + '/public';
-
 app.use(express.static(public_dir));
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  process.env.OPENSHIFT_NODEJS_IP);
+var credentials = {
+    key: fs.readFileSync('auth/server.key'),
+    cert: fs.readFileSync('auth/server.crt')
+};
+
+var httpsServer = https.createServer(credentials, app);
+
+var httpServer = http.createServer(function(req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+});
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
+//app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,
+//  process.env.OPENSHIFT_NODEJS_IP);
